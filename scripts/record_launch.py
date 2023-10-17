5b53af3c22a9e9263255a8c40e7e1a09e74d4874
ernie <simutisernestas@gmail.com>
2023-10-17 08:06:23 +0200

    experiment state fix

--- scripts/record_launch.py
+++ scripts/record_launch.py
@@ -9,9 +9,7 @@ def generate_launch_description():
 
     rec_cmd = '''
         ros2 bag record --compression-mode message --compression-format zstd
-            /fmu/out/sensor_baro
             /fmu/out/sensor_combined
-            /fmu/out/sensor_mag
             /fmu/out/vehicle_attitude
             /fmu/out/vehicle_global_position
             /fmu/out/vehicle_local_position
