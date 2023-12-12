f62d73c4042755f09ab3167a4ae274a7be6f7766
ernie <simutisernestas@gmail.com>
2023-12-12 13:36:44 +0100

    cleanup redundant files

--- launch/sim_bag.launch.py
+++ launch/sim_bag.launch.py
@@ -4,10 +4,47 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 import os
 
+CAM_URDF = """
+<?xml version="1.0"?>
+<robot name="camera_robot">
+
+  <link name="base_link">
+    <visual>
+      <geometry>
+        <cylinder length="0.6" radius="0.2" />
+      </geometry>
+    </visual>
+  </link>
+
+  <joint name="camera_joint" type="fixed">
+    <parent link="base_link" />
+    <child link="camera_link" />
+    <origin xyz="0 0 .2" rpy="0 0.3490658503988659 0" />
+  </joint>
+
+  <link name="camera_link">
+    <visual>
+      <geometry>
+        <box size="0.010 0.03 0.03" />
+      </geometry>
+    </visual>
+  </link>
+
+  <joint name="camera_optical_joint" type="fixed">
+    <origin xyz="0 0 0" rpy="-1.5707963267948966 0 -1.5707963267948966" />
+    <parent link="camera_link" />
+    <child link="camera_link_optical" />
+  </joint>
+
+  <link name="camera_link_optical"></link>
+
+</robot>
+"""
+
 
 def generate_launch_description():
     root_dir = os.path.dirname(
-        os.path.dirname(os.path.realpath(__file__))) # parent; project root
+        os.path.dirname(os.path.realpath(__file__)))  # parent; project root
 
     imu_mag_repub = ExecuteProcess(
         cmd=[f"{root_dir}/src/estimation/build/orientation_filter"],
@@ -41,7 +78,9 @@ def generate_launch_description():
         cmd=['ros2', 'run', 'robot_state_publisher',
              'robot_state_publisher', 'cam.urdf'],
         cwd=f"{root_dir}/assets",
-        output='screen'
+        output='screen',
+        # NOT TESTED !!!
+        parameters=[{'robot_description': CAM_URDF}]
     )
 
     return LaunchDescription([
